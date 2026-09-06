c9735ef0a5083f4bdeb040faa5df96e522b30137
OwPor <rayvincentconcepcion@gmail.com>
2026-09-06 15:32:34 +0800

    chore: strip Flutter app files from web branch (site-only)

--- android/app/src/main/kotlin/com/example/kids_gym_play_space/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kids_gym_play_space
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:play_space/main.dart';
-
-void main() {
-  testWidgets('App starts', (WidgetTester tester) async {
-    await tester.pumpWidget(const PlaySpaceApp());
-    expect(find.text('PlaySpace'), findsOneWidget);
-  });
-}
